2371a17620fc70356038bec42e1e83230d591fd9
aman shah <amanashishshah@gmail.com>
2026-05-14 21:35:16 -0400

    testing into java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.util.Random;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args, int n) {
         // TODO complete this psvm main method to replicate the timing experiment from adts.py.
         //      Note: this will be client code of the various other classes needing to be written.
+
+        int[] itemsAdded;
+        for (int i = 0; i < n; i++) {
+            java.util.Random random = new java.util.Random();
+            int x = random.nextInt(101);
+            // ...
+        }
     }
 }
